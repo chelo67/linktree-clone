5cb6298bba500b91d65a048c315b62491bd56307
chelo67 <marceloacampora@gmail.com>
2025-02-06 21:25:50 -0300

    2:45:02

--- app/(routes)/(home)/components/HandlerSteps/HandlerSteps.tsx
+++ app/(routes)/(home)/components/HandlerSteps/HandlerSteps.tsx
@@ -10,21 +10,23 @@ import {
 import { useStepConfig } from "@/hooks/useStepConfig";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
-import { Progress} from "@/components/ui/progress";
+import { Progress } from "@/components/ui/progress";
+import { StepOne } from "../StepOne";
+
 
 export function HandlerSteps(props: HandlerStepsProps) {
   const { onReload } = props;
 
   const [openDialog, setOpenDialog] = useState(true);
 
-  const {totalSteps, step, setStep} = useStepConfig();
+  const { totalSteps, step, setStep, nextStep } = useStepConfig();
 
   const progressValue = (step / totalSteps) * 100;
 
   const onCloseDialog = () => {
     setOpenDialog(false);
     onReload(true);
-  }
+  };
 
   return (
     <AlertDialog open={openDialog} onOpenChange={setOpenDialog}>
@@ -32,27 +34,26 @@ export function HandlerSteps(props: HandlerStepsProps) {
         <AlertDialogHeader>
           <AlertDialogTitle>
             {step > 1 && step < 5 && (
-                <Button
-                    variant="outline"
-                    className="mr-2"
-                    onClick={() => {}}
-                >
-                    Back <ArrowLeft />
-                </Button>
+              <Button variant="outline" className="mr-2" onClick={() => {}}>
+                Back <ArrowLeft />
+              </Button>
             )}
 
             <div className="mb-2 text-center">
-                step {step} of {totalSteps}
+              step {step} of {totalSteps}
             </div>
 
             <Progress value={progressValue} />
-            
           </AlertDialogTitle>
-          <AlertDialogDescription>
-            {step === 1 && (<p>Step one</p>)}
-            {step === 2 && (<p>Step two</p>)}
-            {step === 3 && (<p>Step three</p>)}
-            {step === 4 && (<p>Step four</p>)}
+          <AlertDialogDescription asChild>
+            <div>
+              {/* <Button onClick={() => nextStep()}>Next Step</Button> */}
+              {step === 1 && <StepOne />}
+              {step === 2 && <p>Step two</p>}
+              {step === 3 && <p>Step three</p>}
+              {step === 4 && <p>Step four</p>}
+              {step === 5 && <p>Step five</p>}
+            </div>
           </AlertDialogDescription>
         </AlertDialogHeader>
       </AlertDialogContent>
---
+++ app/(routes)/(home)/components/StepOne/StepOne.data.ts
@@ -0,0 +1,22 @@
+export const dataCreator = [
+    {
+        title: "✨ Creator",
+        value: "creator",
+    },
+    {
+        title: "🏡 Bussiness",
+        value: "bussiness",
+    },
+    {
+        title: "🧨 Developer",
+        value: "developer"
+    },
+    {
+        title: "🏡 Real State",
+        value: "Real State",
+    },
+    {
+        title: "🏡 Other",
+        value: "Other",
+    },
+]
